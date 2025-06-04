47b7781bad95bc682466361fd7e601e5ed2d1d06
JoginipellyNandhini <161916711+joginipellyNandhini@users.noreply.github.com>
2025-06-04 11:10:34 +0530

    Add files via upload

---
+++ 226726/devlab6726/src/main/java/devlab/devlab6726/App.java
@@ -0,0 +1,13 @@
+package devlab.devlab6726;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
